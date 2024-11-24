578d5fa2684587d259d732a567c037ed9b7b29e0
KMAN-THECHEQUEBOOK <u19061430@tuks.co.za>
2024-11-24 21:21:09 +0200

    initial commit
    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'moeletsiii.dev';
+export class AppComponent implements OnInit {
+  message: string = '';  // Will hold the message to be displayed.
+  fullMessage: string = "I'm an aspiring software engineer"; // Full message
+  currentIndex: number = 0;
+
+  ngOnInit() {
+    this.animateText();
+  }
+
+  animateText() {
+    setInterval(() => {
+      if (this.currentIndex < this.fullMessage.length) {
+        this.message += this.fullMessage[this.currentIndex];
+        this.currentIndex++;
+      }
+    }, 100); // 100ms delay for each letter
+  }
 }
