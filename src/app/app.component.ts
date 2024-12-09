0d706b158f0c41e7a99f3029b544a427b39f92a8
KMAN-THECHEQUEBOOK <u19061430@tuks.co.za>
2024-12-09 07:37:33 +0200

    changes to css
    changes to css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +8,16 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   message: string = '';  // Will hold the message to be displayed.
-  fullMessage: string = "I'm software engineer"; // Full message
+  fullMessage: string = "I'm a software engineer"; // Full message
   currentIndex: number = 0;
+  showModal: boolean = false; // Control the visibility of the modal
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.animateText();
     this.createRain();
   }
-  constructor(private router: Router) {}
 
   goToDisplay1() {
     this.router.navigate(['/display1']);
@@ -23,12 +26,11 @@ export class AppComponent implements OnInit {
   goToDisplay2() {
     this.router.navigate(['/display2']);
   }
-  
-  
+
   createRain() {
     const rainContainer = document.getElementById('rain-container');
     if (!rainContainer) return;
-  
+
     for (let i = 0; i < 100; i++) { // Adjust number of raindrops as needed
       const raindrop = document.createElement('div');
       raindrop.className = 'raindrop';
@@ -38,13 +40,22 @@ export class AppComponent implements OnInit {
       rainContainer.appendChild(raindrop);
     }
   }
-  
+
   animateText() {
     setInterval(() => {
       if (this.currentIndex < this.fullMessage.length) {
         this.message += this.fullMessage[this.currentIndex];
         this.currentIndex++;
       }
-    }, 100); // 100ms delay for each letter
+    }, 200); // 100ms delay for each letter
+  }
+
+  openModal(event: Event) {
+    event.preventDefault(); // Prevent the default link behavior
+    this.showModal = true; // Show the modal
+  }
+
+  closeModal() {
+    this.showModal = false; // Hide the modal
   }
 }
